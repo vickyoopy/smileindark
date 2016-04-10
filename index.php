aa6ce9056c084fc5738cb2c3196642dbf2d09b55
YAO PENG <vickypy0802@gmail.com>
2016-04-11 02:04:16 +0800

    home page design #12
    video css

--- index.php
+++ index.php
@@ -28,11 +28,11 @@
     <!-- large screen display-->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav row">
-            <li><a href="blog/index.php"><img src="imgs/blog.png"><span class="nav-label">Blog</span></a></li>
-            <li><a href="photography/index.php"><img src="imgs/photo.png"><span class="nav-label">Photos</span></a></li>
-            <li><a href="#"><img src="imgs/home.png" style="bottom: -2em"><span class="nav-label">Home</span></a></li>
-            <li><a href="admin/index.php"><img src="imgs/admin.png"> <span class="nav-label">Admin</span> </a></li>
-            <li><a href="contact/index.php"><img src="imgs/contact.png"><span class="nav-label">Contact</span></a></li>
+            <li class="col-sm-2"><a href="blog/index.php"><img src="imgs/blog.png"><span class="nav-label">Blog</span></a></li>
+            <li class="col-sm-2"><a href="photography/index.php"><img src="imgs/photo.png"><span class="nav-label">Photos</span></a></li>
+            <li class="col-sm-2"><a href="#"><img src="imgs/home.png" style="bottom: -3em"><span class="nav-label">Home</span></a></li>
+            <li class="col-sm-2"><a href="admin/index.php"><img src="imgs/admin.png"> <span class="nav-label">Admin</span> </a></li>
+            <li class="col-sm-2"><a href="contact/index.php"><img src="imgs/contact.png"><span class="nav-label">Contact</span></a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -40,13 +40,19 @@
 
 
 <!-- Begin Wrapper -->
-    <section id="section1" class="cd-section">
-        <h1>- SMILE IN DARK -</h1><br>
-        <a href="#section2" class="cd-scroll-down cd-img-replace" onclick="s2load()">scroll down</a>
+    <section class="cd-section">   
+    <div class="row">
+        <div class="col-sm-12 col-xs-12 col-md-2" > <img src="imgs/elec.png" style="width:160%;float: right;margin-right: -200px;z-index:-1" > video information </div>
+        <div class="col-sm-12 col-xs-12 col-md-8" style="z-index:999;">
+            <div class="row"><img src="imgs/videobg.png" style="height:100px;margin-bottom: -10px;" ></div>
+            <div class="row"><embed src="http://static.hdslb.com/play.swf?aid=2825096" width="960" height="540"></embed></div>
+        </div>
+        <div class="col-sm-12 col-xs-12 col-md-2"> <img src="imgs/audacity.png" style="float:left;margin:400px -180px;z-index: -1;">click to switch video</div> 
+    </div>
     </section>
 
-    <section id="section2" class="cd-section">
-        <p id="s2">If you don't fail, you are not even trying.</p>
+    <section class="cd-section">
+        
     </section>
 
 
@@ -55,7 +61,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-            copyright©2014-2025 | Vicky.P.Y | <a href="mailto:vickypy0802@gmail.com?subject=Hello,Vicky"> Email Me</a> 
+            copyright婕�2014-2025 | Vicky.P.Y | <a href="mailto:vickypy0802@gmail.com?subject=Hello,Vicky"> Email Me</a> 
             </div>
         </div>
     </div>
@@ -73,4 +79,4 @@
     </script>
 
 </body>
-</html>
+</html>                
